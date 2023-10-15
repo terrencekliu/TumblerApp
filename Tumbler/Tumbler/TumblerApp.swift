c497025fa4ca4f8d88b04ec230183bae432743b5
terrenceliu <tliu29@calpoly.edu>
2023-10-15 14:29:24 -0700

    Setup inital project

---
+++ Tumbler/Tumbler/TumblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TumblerApp.swift
+//  Tumbler
+//
+//  Created by Terrence Liu on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TumblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tumbler/TumblerUITests/TumblerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TumblerUITestsLaunchTests.swift
+//  TumblerUITests
+//
+//  Created by Terrence Liu on 10/15/23.
+//
+
+import XCTest
+
+final class TumblerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
