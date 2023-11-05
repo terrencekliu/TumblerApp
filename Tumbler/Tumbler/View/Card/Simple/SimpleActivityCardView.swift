8b465e196026229cf784dc941a703bd70da3ae3e
terrenceliu <tliu29@calpoly.edu>
2023-11-04 22:42:00 -0700

    SimpleTripCardView skeleton code added

---
+++ Tumbler/Tumbler/View/Card/Simple/SimpleActivityCardView.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleActivityCardView.swift
+//  Tumbler
+//
+//  Created by Terrence Liu on 11/4/23.
+//
+
+import SwiftUI
+
+struct SimpleActivityCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleActivityCardView()
+}
