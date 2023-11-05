a3233581b96ca969bd4d3d0e1d4af3caa5a93465
terrenceliu <tliu29@calpoly.edu>
2023-11-04 22:55:04 -0700

    Fixed code

--- Tumbler/Tumbler/View/Card/Simple/SimpleActivityCardView.swift
+++ Tumbler/Tumbler/View/Card/Simple/SimpleActivityCardView.swift
@@ -1,18 +1,37 @@
 //
-//  SimpleActivityCardView.swift
+//  SimpleTripCardView.swift
 //  Tumbler
 //
-//  Created by Terrence Liu on 11/4/23.
+//  Created by Carol Liu on 11/4/23.
 //
 
 import SwiftUI
 
-struct SimpleActivityCardView: View {
+struct SimpleTripCardView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topLeading) {
+
+            Image("rectangleCafe")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 350, height: 199)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .brightness(-0.2)
+            VStack(alignment: .leading) {
+                Text("Barcelona")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Text("Feb 6 - 12, 2020")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+            } .padding()
+        }
     }
 }
 
 #Preview {
-    SimpleActivityCardView()
+    SimpleTripCardView()
 }
