cd7eb322c4150e98e6568cfd2fa90fe8fae22d52
Vincent Liu <alizarinaurora@gmail.com>
2024-01-23 21:55:50 -0800

    Add DetailedActivityView. Add Create New Trip button in TripTabView.

---
+++ Tumbler/Tumbler/View/Card/Full/FullActivity/DetailedActivitiyView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedActivitiyView.swift
+//  Tumbler
+//
+//  Created by Vincent Liu on 1/23/24.
+//
+
+import SwiftUI
+
+struct DetailedActivitiyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedActivitiyView()
+}
