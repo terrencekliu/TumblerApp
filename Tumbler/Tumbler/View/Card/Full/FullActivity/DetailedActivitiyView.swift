1937cbe15da051351fd5e4a2754fb4bf739f60ed
terrenceliu <tliu29@calpoly.edu>
2024-02-03 23:32:53 -0800

    Refactor DetailedActivityView

--- Tumbler/Tumbler/View/Card/Full/FullActivity/DetailedActivitiyView.swift
+++ Tumbler/Tumbler/View/Card/Full/FullActivity/DetailedActivitiyView.swift
@@ -81,50 +81,13 @@ private let pizza = Activity(
     alert: "Car break-in common"
 )
 
-// TODO: May need to put type view for different symbols settings
 enum ActivitySymbolName: String {
-    case attraction = "fork.knife.circle.fill"
-    case food = "fork.knife.circle.fil"
-    case beach = "fork.knife.circle.fi"
-    case house = "fork.knife.circle.f"
-    case camp = "fork.knife.circle."
-    case other = "fork.knife.circle"
-}
-
-enum ActivityTypePlural: String {
-    case attractions
-    case foods
-    case beaches
-    case houses
-    case camps
-    case others
-}
-
-struct ActivityGroup: View {
-    @State var activities: [Activity]
-    let symbol: ActivitySymbolName
-    let activityText: ActivityTypePlural
-
-    var body: some View {
-        if !activities.isEmpty {
-            VStack {
-                HStack {
-                    Image(systemName: "\(symbol.rawValue)")
-                        .symbolRenderingMode(.multicolor)
-                        .font(.system(size: 45))
-                    Text("\(activityText.rawValue.capitalized)")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                .padding(.leading, 6)
-                Divider()
-                ForEach(activities, id: \.id) { attraction in
-                    ActivityRow(activity: attraction)
-                }
-            }
-        }
-    }
+    case attractions = "fork.knife.circle.fill"
+    case foods = "fork.knife.circle.fil"
+    case beaches = "fork.knife.circle.fi"
+    case houses = "fork.knife.circle.f"
+    case camps = "fork.knife.circle."
+    case others = "fork.knife.circle"
 }
 
 struct DetailedActivitiyView: View {
@@ -137,34 +100,14 @@ struct DetailedActivitiyView: View {
     @State private var others: [Activity] = []
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(spacing: 30) {
-                    ActivityGroup(activities: attractions,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.attractions)
-                    
-                    ActivityGroup(activities: foods,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.foods)
-                    
-                    ActivityGroup(activities: beaches,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.beaches)
-                    
-                    ActivityGroup(activities: houses,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.houses)
-                    
-                    ActivityGroup(activities: camps,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.camps)
-                    
-                    ActivityGroup(activities: others,
-                                  symbol: ActivitySymbolName.attraction,
-                                  activityText: ActivityTypePlural.others)
-                }
-                .padding(.top, 30)
+        NavigationStack {
+            List {
+                if !attractions.isEmpty { section(attractions, ActivitySymbolName.attractions) }
+                if !foods.isEmpty { section(foods, ActivitySymbolName.foods) }
+                if !beaches.isEmpty { section(beaches, ActivitySymbolName.beaches) }
+                if !houses.isEmpty { section(houses, ActivitySymbolName.houses) }
+                if !camps.isEmpty { section(camps, ActivitySymbolName.camps) }
+                if !others.isEmpty { section(others, ActivitySymbolName.others) }
             }
             .navigationTitle("Activities")
             .toolbar {
@@ -180,6 +123,28 @@ struct DetailedActivitiyView: View {
             }
         }
     }
+
+    private func section(_ activities: [Activity], _ type: ActivitySymbolName) -> some View {
+        let name = "\(type)"
+
+        return SwiftUI.Section(header:
+            HStack {
+                Image(systemName: "\(type.rawValue)")
+                    .symbolRenderingMode(.multicolor)
+                    .font(.system(size: 45))
+                Text(name.capitalized)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            }
+            .textCase(nil)
+            .padding(.leading, -20.0)
+        ) {
+            ForEach(attractions, id: \.id) { attraction in
+                ActivityRow(activity: attraction)
+            }
+        }
+    }
 }
 
 struct ActivityRow: View {
@@ -187,28 +152,21 @@ struct ActivityRow: View {
     @State var activity: Activity
 
     var body: some View {
-        HStack {
-            Button(action: {}) {
-                Image("rectangleCafe")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 35)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                    .brightness(-0.2)
-                    .accessibilityIdentifier("preview-image")
-                Text(activity.name)
-                    .foregroundStyle(.black)
-                Spacer()
-                Image(systemName: "chevron.right")
-                    .foregroundColor(.secondary)
-                    .symbolRenderingMode(.monochrome)
-            }
-            .accessibilityIdentifier("new-trip-button")
-
-            Spacer()
+        NavigationLink {
+            
+        } label: {
+            Label(
+                title: { Text(activity.name).foregroundStyle(.black) },
+                icon: { Image("rectangleCafe")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 35)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .brightness(-0.2)
+                        .accessibilityIdentifier("preview-image")
+                }
+            )
         }
-        .padding([.leading, .trailing])
-        Divider()
     }
 }
 
